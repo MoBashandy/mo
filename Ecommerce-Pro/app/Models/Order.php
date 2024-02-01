065e14d885a6711fa88ed6b08ae974c47d962929
mo <bemobashandy@gmail.com>
2024-02-01 12:01:49 +0200

    Fix Some Issues

--- Ecommerce-Pro/app/Models/Order.php
+++ Ecommerce-Pro/app/Models/Order.php
@@ -4,9 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Notifications\Notifiable;
+
 class Order extends Model
 {
     use HasFactory;
-    use Notifiable;
 }
--- Ecommerce-Pro/database/migrations/2024_02_01_094735_create_carts_table.php
+++ Ecommerce-Pro/database/migrations/2024_02_01_094735_create_carts_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
-            // $table->string('name')->nullable();
             $table->string('email')->nullable();
+            $table->string('name')->nullable();
             $table->string('phone')->nullable();
             $table->string('address')->nullable();
             $table->string('product_title')->nullable();
