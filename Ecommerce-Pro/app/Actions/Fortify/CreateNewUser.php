ed7d40a84ce63304439b685b370f0d0bb9bc48eb
mo <bemobashandy@gmail.com>
2024-02-01 15:27:06 +0200

    Edit My project

--- Ecommerce-Pro/app/Actions/Fortify/CreateNewUser.php
+++ Ecommerce-Pro/app/Actions/Fortify/CreateNewUser.php
@@ -29,10 +29,7 @@ class CreateNewUser implements CreatesNewUsers
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
-            'phone' => $input['phone'],
-            'address' => $input['address'],
             'password' => Hash::make($input['password']),
-
         ]);
     }
 }
--- Ecommerce-Pro/resources/views/navigation-menu.blade.php
+++ Ecommerce-Pro/resources/views/navigation-menu.blade.php
@@ -3,19 +3,19 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <!-- Logo
+                <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
                         <x-application-mark class="block h-9 w-auto" />
                     </a>
                 </div>
 
-                {{-- <!-- Navigation Links --> --}}
+                <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                </div>-->
+                </div>
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ms-6">
